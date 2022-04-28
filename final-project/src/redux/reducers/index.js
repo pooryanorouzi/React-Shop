c0ba66d8404939e6fdc464b95d11d0dbbfbb3f8e
pooryanorouzi <sezar77.ps@gmail.com>
2022-04-29 00:46:07 +0430

    fonts add

---
+++ final-project/src/redux/reducers/index.js
@@ -0,0 +1,4 @@
+export { CategoryReducer } from "./category.reducer";
+export { ProductsReducer } from "./product.reducer";
+export { CartReducer } from "./cart.reducer";
+export { OrdersReducer } from "./orders.reducer";
